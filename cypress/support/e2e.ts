691c948316079fad941ce68c4f5bf17b44fd7576
Michael Bruty <57625243+mbruty@users.noreply.github.com>
2023-01-24 18:08:39 +0000

    Update e2e.ts

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -14,7 +14,6 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
 import * as mongo from 'cypress-mongodb';
 
 mongo.addCommands();
